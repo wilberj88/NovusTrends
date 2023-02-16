20f00eaa219172b1fa03794fa71925c6eb6299d4
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-16 10:57:43 -0500

    Create bing.py

---
+++ pages/bing.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Trends", page_icon="⚙️")
+
+st.title('Novus Trends ⚙️')
+st.header("Monitores de Tendencias en Microsoft BING en tiempo real💹")
+
+st.write("Elige tendencia y ubicación. Nuestra tecnología hará el resto")
+
+st.markdown(
+  """
+  EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
+  """
+)
