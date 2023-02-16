3ffc66d0264c6e025ba15e01ca33e37c65227987
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-16 12:16:56 -0500

    Update google.py

--- pages/google.py
+++ pages/google.py
@@ -19,19 +19,17 @@ st.write("Tendencias Hoy en Estados Unidos 🇺🇸")
 df1 = pytrends.trending_searches(pn='united_states')
 st.dataframe(df1.head(10))
 
+st.write("Tendencias Hoy en Colombia 🇨🇴")
+# Google Trends data
+df3 = pytrends.trending_searches(pn='colombia')
+st.dataframe(df3.head(10))
 
 st.write("Tendencias Hoy en Nigeria 🇳🇬")
 # Google Trends data
 df4 = pytrends.trending_searches(pn='nigeria')
 st.dataframe(df4.head(10))
 
-st.write("Tendencias Hoy en Colombia 🇨🇴")
-# Google Trends data
-df3 = pytrends.trending_searches(pn='colombia')
-st.dataframe(df3.head(10))
-
-pytrends1 = TrendReq(hl='es', tz=360)
 st.write("Tendencias Hoy en España 🇪🇸")
 # Google Trends data
-df2 = pytrends1.trending_searches(pn='spain')
+df2 = pytrends.trending_searches(pn='spain')
 st.dataframe(df2.head(10))
