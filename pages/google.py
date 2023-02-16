9b2cd05ba0fbc4399eb6f324f36af7aaa307ab16
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-16 11:46:19 -0500

    Update google.py

--- pages/google.py
+++ pages/google.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pytrends
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+from pytrends.request import TrendReq
+
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="Novus Trends", page_icon="⚙️")
