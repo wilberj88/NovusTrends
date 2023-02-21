a207ee0b4266c33dea5dd6659bc12e6f2996ec23
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-21 11:20:55 -0500

    Update linkedin.py

--- pages/linkedin.py
+++ pages/linkedin.py
@@ -1,1 +1,14 @@
 import streamlit as st
+import pytrends
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pytrends.request import TrendReq
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Trends", page_icon="⚙️")
+
+st.title('Novus Trends ⚙️')
+st.header("Monitores de Tendencias en LinkedIn en tiempo real💹")
