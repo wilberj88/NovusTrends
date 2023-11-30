e5237a43dfbc1819e8dd186b7fcc7d74b8468e1a
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-30 20:28:42 +0100

    Update tecnaliaimpacto.py

--- pages/tecnaliaimpacto.py
+++ pages/tecnaliaimpacto.py
@@ -531,20 +531,3 @@ if a == "Beneficiarios":
     col2.metric(label="Salario mensual", value=f"£ {player_stats['Wage(£)']:,}")
     col3.metric(label="Cláusula de recisión", value=f"£ {player_stats['Release Clause(£)']:,}")
     
-    df_filtered = df_data[df_data["Club"] == club].set_index("Name")
-    
-    st.image(df_filtered.iloc[0]["Club Logo"])
-    st.markdown(f"## {club}")
-    
-    columns = ["Age", "Photo", "Flag", "Overall", "Value(£)", "Wage(£)", "Joined",
-               "Height(cm.)", "Weight(lbs.)", "Contract Valid Until", "Release Clause(£)"]
-    
-    st.dataframe(df_filtered[columns],
-                 column_config={
-                   "Overall": st.column_config.ProgressColumn("Overall", format="%d", min_value=0, max_value=100),
-                   "Value(£)": st.column_config.NumberColumn(),
-                   "Wage(£)": st.column_config.ProgressColumn("Weekly Wage", format="£%f",
-                                                              min_value=0, max_value=df_filtered["Wage(£)"].max()),
-                   "Photo": st.column_config.ImageColumn(),
-                   "Flag": st.column_config.ImageColumn("Country"),                 
-                 }, height=1000)
