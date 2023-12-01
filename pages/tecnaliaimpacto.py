18058f01f352a8284b82e0642358a1d577512d14
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-12-01 17:46:07 +0100

    Update tecnaliaimpacto.py

--- pages/tecnaliaimpacto.py
+++ pages/tecnaliaimpacto.py
@@ -501,33 +501,33 @@ if a == "Beneficiarios":
     df_data = df_data[df_data["Value(£)"] > 0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
     st.session_state["data"] = df_data
-
     clubes = df_data["Club"].value_counts().index
-    club = st.sidebar.selectbox("Club", clubes)
-    
     df_players = df_data[df_data["Club"] == club]
     players = df_players["Name"].value_counts().index
-    player = st.sidebar.selectbox("Jugador", players)
-    
-    player_stats = df_data[df_data["Name"] == player].iloc[0]
-    
-    st.image(player_stats["Photo"])
-    st.title(f"{player_stats['Name']}")
-    
-    st.markdown(f"**Equipo:** {player_stats['Club']}")
-    st.markdown(f"**Rol:** {player_stats['Position']}")
-    
-    col1, col2, col3, col4 = st.columns(4)
-    col1.markdown(f"**Edad:** {player_stats['Age']}")
-    col2.markdown(f"**Altura:** {player_stats['Height(cm.)']/100}")
-    col3.markdown(f"**Peso:** {player_stats['Weight(lbs.)']*0.453:.2f}")
-    
-    st.divider()
-    st.subheader(f"Overal {player_stats['Overall']}")
-    st.progress(int(player_stats['Overall']))
-    
-    col1, col2, col3 = st.columns(3)
-    col1.metric(label="Valor de mercado", value=f"£ {player_stats['Value(£)']:,}")
-    col2.metric(label="Salario mensual", value=f"£ {player_stats['Wage(£)']:,}")
-    col3.metric(label="Cláusula de recisión", value=f"£ {player_stats['Release Clause(£)']:,}")
+
+    x = st.selectbox("Comunidad Autónoma", clubes)
+    if x:
+        player = st.selectbox("Empresa", players)
+        
+        player_stats = df_data[df_data["Name"] == player].iloc[0]
+        
+        st.image(player_stats["Photo"])
+        st.title(f"{player_stats['Name']}")
+        
+        st.markdown(f"**Equipo:** {player_stats['Club']}")
+        st.markdown(f"**Rol:** {player_stats['Position']}")
+        
+        col1, col2, col3, col4 = st.columns(4)
+        col1.markdown(f"**Edad:** {player_stats['Age']}")
+        col2.markdown(f"**Altura:** {player_stats['Height(cm.)']/100}")
+        col3.markdown(f"**Peso:** {player_stats['Weight(lbs.)']*0.453:.2f}")
+        
+        st.divider()
+        st.subheader(f"Overal {player_stats['Overall']}")
+        st.progress(int(player_stats['Overall']))
+        
+        col1, col2, col3 = st.columns(3)
+        col1.metric(label="Valor de mercado", value=f"£ {player_stats['Value(£)']:,}")
+        col2.metric(label="Salario mensual", value=f"£ {player_stats['Wage(£)']:,}")
+        col3.metric(label="Cláusula de recisión", value=f"£ {player_stats['Release Clause(£)']:,}")
     
